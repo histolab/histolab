0dc7fdc9cc5ee83bd20afce550c6c8d01d1e566d
ernestoarbitrio <ernesto.arbitrio@gmail.com>
2020-06-12 00:05:22 +0200

    tests: unit tests for data module

---
+++ tests/unit/data/test_data.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+import os
+
+import src.histolab.data as data
+
+
+def test_data_dir():
+    # data_dir should be a directory that can be used as a standard directory
+    data_dir = data.data_dir
+    assert "cmu_small_region.svs" in os.listdir(data_dir)
+
+
+def test_cmu_small_region():
+    """ Test that "cmu_small_region" svs can be loaded. """
+    cmu_small_region = data.cmu_small_region()
+    assert cmu_small_region.dimensions == (2220, 2967)
