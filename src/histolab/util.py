8ce553abc89e00d20e76999ac024a8200b0554ed
Alessia Marcolini <amarcolini@fbk.eu>
2020-06-16 22:02:43 +0200

    Fix docstring

--- src/histolab/util.py
+++ src/histolab/util.py
@@ -67,18 +67,17 @@ def scale_coordinates(
 
     Parameters
     ----------
-    reference_coords: tuple, array-like or Coordinates
-        (x, y) pair of coordinates referring to the upper left and lower right corners
-        respectively. The function expects a tuple of four elements or a tuple of two
-        pairs of coordinates as input.
-    reference_size: array_like of int
+    reference_coords: CoordinatePair
+        Coordinates referring to the upper left and lower right corners
+        respectively.
+    reference_size: tuple of int
         Reference (width, height) size to which input coordinates refer to
-    target_size: array_like of int
+    target_size: tuple of int
         Target (width, height) size of the resulting scaled image
 
     Returns
     -------
-    coords: Coordinates
+    coords: CoordinatesPair
         Coordinates in the scaled image
     """
     reference_coords = np.asarray(reference_coords).ravel()
