5e5c1a4657e04a46e61cbc8ba7a8450058b4b0c4
ernestoarbitrio <ernesto.arbitrio@gmail.com>
2020-06-19 09:37:29 +0200

    release: prepare v0.0.3b release

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ install_requires = [
     "scipy",
     "sparse",
     "openslide-python",
+    "setuptools==45",
 ]
 
 test_requires = ["pytest", "coverage", "pytest-cov==2.8.1", "coveralls"]
@@ -49,9 +50,6 @@ setuptools.setup(
     url="https://github.com/MPBA/histolab",
     download_url="https://pypi.python.org/pypi/histolab",
     install_requires=install_requires,
-    dependency_links=[
-        "https://github.com/openslide/openslide-python/archive/master.zip#egg=openslide-python"  # noqa
-    ],
     tests_require=test_requires,
     extras_require={"testing": test_requires},
     packages=setuptools.find_packages("src", exclude=["tests"]),
