a81b99dd5e8612d7fbf5401c3fed4dd4171a2184
alessiamarcolini <98marcolini@gmail.com>
2020-08-04 20:41:18 +0200

    fix escaping on windows path

--- tests/unit/test_slide.py
+++ tests/unit/test_slide.py
@@ -217,7 +217,7 @@ class Describe_Slide(object):
         assert isinstance(err.value, PIL.UnidentifiedImageError)
         assert (
             str(err.value) == f"cannot identify image file "
-            rf"'{os.path.normpath(os.path.join(slide_path))}'"
+            f"'{str(os.path.join(slide_path))}'"
         )
 
     def it_can_resample_itself(self, tmpdir, resampled_dims_):
@@ -445,7 +445,7 @@ class Describe_Slide(object):
         assert (
             str(err.value)
             == "Cannot display the slide thumbnail:[Errno 2] No such file or "
-            rf"directory: '{os.path.join('processed', 'thumbnails', 'b.png')}'"
+            f"directory: '{str(os.path.join('processed', 'thumbnails', 'b.png'))}'"
         )
 
     def it_knows_its_level_dimensions(self, tmpdir):
