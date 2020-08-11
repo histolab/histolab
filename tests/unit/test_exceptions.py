e46beb9cdf0a3896bb96229777cafd9b0250e8d0
alessiamarcolini <98marcolini@gmail.com>
2020-08-12 00:15:58 +0200

    Refactor exceptions tests according to CR comments

--- tests/unit/test_exceptions.py
+++ tests/unit/test_exceptions.py
@@ -6,51 +6,72 @@ from ..unitutil import ANY, initializer_mock
 
 
 class DescribeExceptions(object):
-    @pytest.mark.parametrize("args", ("error", ["hello", "error"], None))
-    def it_can_construct_from_args_level_error(self, request, args):
+    @pytest.mark.parametrize("arg", (["hello", "error"], None))
+    def it_can_construct_from_str_or_none_level_error(self, request, arg):
         _init = initializer_mock(request, exp.LevelError)
 
-        if type(args) == list:
-            level_error = exp.LevelError(*args)
-        else:
-            level_error = exp.LevelError(args)
+        level_error = exp.LevelError(arg)
 
-        if type(args) == list:
-            _init.assert_called_once_with(ANY, *args)
-        else:
-            _init.assert_called_once_with(ANY, args)
+        _init.assert_called_once_with(ANY, arg)
         assert isinstance(level_error, exp.LevelError)
         assert isinstance(level_error, Exception)
 
-    @pytest.mark.parametrize("args", ("error", ["hello", "error"], None))
-    def it_knows_its_message_level_error(self, request, args):
-        if type(args) == list:
-            level_error = exp.LevelError(*args)
-        else:
-            level_error = exp.LevelError(args)
+    def it_can_construct_from_list_level_error(self, request):
+        _init = initializer_mock(request, exp.LevelError)
+        args = ["hello", "error"]
+
+        level_error = exp.LevelError(*args)
+
+        _init.assert_called_once_with(ANY, *args)
+        assert isinstance(level_error, exp.LevelError)
+        assert isinstance(level_error, Exception)
+
+    def it_knows_its_message_from_str_level_error(self, request):
+        arg = "error"
+        level_error = exp.LevelError(arg)
 
         message = level_error.message
 
-        if args:
-            if type(args) == str:
-                assert type(message) == str
-                assert message == args
-            if type(args) == list:
-                assert type(message) == str
-                assert message == list(args)[0]
-        else:
-            assert message is None
+        assert type(message) == str
+        assert message == arg
 
-    @pytest.mark.parametrize("args", ("error", ["hello", "error"], None))
-    def it_knows_its_str_level_error(self, request, args):
-        if type(args) == list:
-            level_error = exp.LevelError(*args)
-        else:
-            level_error = exp.LevelError(args)
+    def it_knows_its_message_from_list_level_error(self, request):
+        args = ["hello", "error"]
+        level_error = exp.LevelError(*args)
+
+        message = level_error.message
+
+        assert type(message) == str
+        assert message == list(args)[0]
+
+    def it_knows_its_message_from_none_level_error(self, request):
+        arg = None
+        level_error = exp.LevelError(arg)
+
+        message = level_error.message
+
+        assert message is None
+
+    def it_knows_its_str_from_str_level_error(self, request):
+        arg = "error"
+        level_error = exp.LevelError(arg)
 
         s = str(level_error)
 
-        if not args:
-            assert s == ""
-        else:
-            assert s == level_error.message
+        assert s == level_error.message
+
+    def it_knows_its_str_from_list_level_error(self, request):
+        args = ["hello", "error"]
+        level_error = exp.LevelError(*args)
+
+        s = str(level_error)
+
+        assert s == level_error.message
+
+    def it_knows_its_str_from_none_level_error(self, request):
+        arg = None
+        level_error = exp.LevelError(arg)
+
+        s = str(level_error)
+
+        assert s == ""
