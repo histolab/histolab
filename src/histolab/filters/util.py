57ea9f6dce77f84ffc8e9f97d9a9b2effc955f8c
alessiamarcolini <98marcolini@gmail.com>
2020-08-30 14:55:59 +0200

    Add mask differnce function

--- src/histolab/filters/util.py
+++ src/histolab/filters/util.py
@@ -17,3 +17,23 @@ def mask_percent(mask: np.ndarray) -> float:
 
     mask_percentage = 100 - np.count_nonzero(mask) / mask.size * 100
     return mask_percentage
+
+
+def mask_difference(minuend: np.ndarray, subtrahend: np.ndarray) -> np.ndarray:
+    """Return the element-wise difference between two binary masks.
+
+    Parameters
+    ----------
+    minuend : np.ndarray
+        The mask from which another is subtracted
+    subtrahend : np.ndarray
+        The mask that is to be subtracted
+
+    Returns
+    -------
+    np.ndarray
+        The element-wise difference between the two binary masks
+    """
+    difference = minuend.astype("int8") - subtrahend.astype("int8")
+    difference[difference == -1] = 0
+    return difference.astype("bool")
