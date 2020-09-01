abd9f3eabbcd019e25286508b1d431f7636abf75
alessiamarcolini <98marcolini@gmail.com>
2020-09-01 16:23:00 +0200

    Add unit and integration tests for NucleiScorer

---
+++ tests/integration/test_scorer.py
@@ -0,0 +1,28 @@
+import pytest
+
+from histolab import scorer
+from histolab.tile import Tile
+
+from ..fixtures import TILES
+
+
+class Describe_Scorers(object):
+    @pytest.mark.parametrize(
+        "tile_img, expected_score",
+        (
+            (TILES.VERY_LOW_NUCLEI_SCORE_LEVEL0, 0.39668202921529044),
+            (TILES.LOW_NUCLEI_SCORE_LEVEL0, 0.34299989754219434),
+            (TILES.MEDIUM_NUCLEI_SCORE_LEVEL0, 0.3832490771628042),
+            (TILES.HIGH_NUCLEI_SCORE_LEVEL0, 0.7147274900858304),
+            (TILES.MEDIUM_NUCLEI_SCORE_LEVEL2, 0.5226490280587772),
+            (TILES.HIGH_NUCLEI_SCORE_LEVEL2, 0.5941818245460105),
+            (TILES.NO_TISSUE, 0.015577231690474633),
+        ),
+    )
+    def it_knows_nuclei_score(self, tile_img, expected_score):
+        tile = Tile(tile_img, None)
+        nuclei_scorer = scorer.NucleiScorer()
+
+        score = nuclei_scorer(tile, 0.8)
+
+        assert score == expected_score
--- tests/unit/test_scorer.py
+++ tests/unit/test_scorer.py
@@ -1,7 +1,17 @@
+import numpy as np
+import PIL
+
 from histolab import scorer
 from histolab.tile import Tile
 
-from ..unitutil import instance_mock
+from ..base import COMPLEX_MASK
+from ..unitutil import (
+    PILImageMock,
+    function_mock,
+    instance_mock,
+    method_mock,
+    property_mock,
+)
 
 
 class DescribeScorers(object):
@@ -14,3 +24,32 @@ class DescribeScorers(object):
         assert isinstance(random_scorer, scorer.RandomScorer)
         assert isinstance(random_scorer, scorer.Scorer)
         assert type(score) == float
+
+    def it_can_construct_nuclei_scorer(self, request):
+        image = PILImageMock.DIMS_10X10_RGB_RANDOM_COLOR
+        tissue_ratio_ = property_mock(request, Tile, "tissue_ratio")
+        tissue_ratio_.return_value = 0.7
+        apply_filters_ = method_mock(request, Tile, "apply_filters")
+        apply_filters_.return_value = Tile(PIL.Image.fromarray(COMPLEX_MASK), None, 0)
+        mask_difference_ = function_mock(request, "histolab.scorer.mask_difference")
+        mask_difference_.return_value = np.zeros_like(COMPLEX_MASK).astype("bool")
+        tile = Tile(image, None, 0)
+        nuclei_scorer = scorer.NucleiScorer()
+
+        score = nuclei_scorer(tile, 0.6)
+
+        tissue_ratio_.assert_called_once()
+        assert len(apply_filters_.call_args_list) == 2
+        # not possible to test filters compositions instances used in the call
+        assert apply_filters_.call_args_list[0][0][0] == tile
+        assert apply_filters_.call_args_list[1][0][0] == tile
+        np.testing.assert_array_equal(
+            mask_difference_.call_args_list[0][0][0], COMPLEX_MASK
+        )
+        np.testing.assert_array_equal(
+            mask_difference_.call_args_list[0][0][1], COMPLEX_MASK
+        )
+        assert isinstance(nuclei_scorer, scorer.NucleiScorer)
+        assert isinstance(nuclei_scorer, scorer.Scorer)
+        assert type(score) == float
+        assert score == 0.7 * 0.4  # to avoid float representation issues
