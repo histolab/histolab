2c8ead4665e6082c7c75c4274015f15f0e495be3
alessiamarcolini <98marcolini@gmail.com>
2020-09-02 20:15:58 +0200

    Add more test cases to mask_difference test

--- tests/unit/filters/test_util.py
+++ tests/unit/filters/test_util.py
@@ -1,16 +1,34 @@
 import numpy as np
+import pytest
 
 from histolab.filters.util import mask_difference
 
-from ...base import BASE_MASK, BASE_MASK2
+from ...base import (
+    BASE_MASK,
+    BASE_MASK2,
+    BASE_MASK3,
+    BASE_MASK4,
+    COMPLEX_MASK,
+    COMPLEX_MASK2,
+    COMPLEX_MASK3,
+)
 
 
-def test_mask_difference():
-    expected_mask_difference = np.array(
-        [[False, True, False, False], [False, False, False, False]]
-    )
-
-    mask_difference_ = mask_difference(BASE_MASK, BASE_MASK2)
+@pytest.mark.parametrize(
+    "array1, array2, expected_mask_difference",
+    (
+        (
+            BASE_MASK,
+            BASE_MASK2,
+            np.array([[False, True, False, False], [False, False, False, False]]),
+        ),
+        (BASE_MASK3, BASE_MASK4, np.array([True, False, False, False])),
+        (COMPLEX_MASK, COMPLEX_MASK2, COMPLEX_MASK3),
+    ),
+)
+def test_mask_difference(array1, array2, expected_mask_difference):
+    mask_difference_ = mask_difference(array1, array2)
+    print(mask_difference_)
 
     assert type(mask_difference_) == np.ndarray
     assert mask_difference_.dtype == "bool"
