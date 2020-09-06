9e4ae250cec038fab15314d0df9592fc65ffee3b
ernestoarbitrio <ernesto.arbitrio@gmail.com>
2020-09-06 12:54:03 +0200

    get rid of tissue_percent: unused

--- src/histolab/filters/util.py
+++ src/histolab/filters/util.py
@@ -1,5 +1,4 @@
 import numpy as np
-from PIL import Image
 
 
 def mask_percent(mask: np.ndarray) -> float:
@@ -18,19 +17,3 @@ def mask_percent(mask: np.ndarray) -> float:
 
     mask_percentage = 100 - np.count_nonzero(mask) / mask.size * 100
     return mask_percentage
-
-
-def tissue_percent(img: Image.Image) -> float:
-    """Compute percentage of tissue in an image.
-
-    Parameters
-    ----------
-    img : Image.Image
-        Input image
-
-    Returns
-    -------
-    float
-        Percentage of image that is not masked
-    """
-    return 100 - mask_percent(img)
