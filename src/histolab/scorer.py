cf6291d8594d062ebe1c8b6b194a52787ee226bf
Nicole Bussola <nicola.bussolaceradini@gmail.com>
2021-02-25 22:56:50 +0100

    remove comments

--- src/histolab/scorer.py
+++ src/histolab/scorer.py
@@ -67,7 +67,8 @@ class RandomScorer(Scorer):
 
 
 class NucleiScorer(Scorer):
-    r"""Implement a Scorer that estimates the presence of nuclei in an H&E-stained tile.
+    r"""This class implements an hybrid algorithm that combines thresholding and
+    morphological operations to segment nuclei on H&E-stained histological images.
 
     This class implements an hybrid algorithm that combines thresholding and
     morphological operations to segment nuclei on H&E-stained histological images.
