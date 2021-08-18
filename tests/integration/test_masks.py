9963e6149e3cf45ea430e1c6b2a1c60d5c9ca64f
alessiamarcolini <98marcolini@gmail.com>
2021-08-18 16:38:44 +0200

    Add unit and integration tests

--- tests/integration/test_masks.py
+++ tests/integration/test_masks.py
@@ -7,8 +7,9 @@ import pytest
 
 from histolab.masks import BiggestTissueBoxMask, TissueMask
 from histolab.slide import Slide
+from histolab.tile import Tile
 
-from ..fixtures import SVS, TIFF
+from ..fixtures import RGB, SVS, TIFF, TILES
 from ..util import load_expectation
 
 
@@ -56,8 +57,37 @@ class DescribeTissueMask:
             ),
         ),
     )
-    def it_can_construct_tissue_mask(self, wsi, expected_array):
+    def it_can_construct_tissue_mask_slide(self, wsi, expected_array):
         slide = Slide(wsi, os.path.join(wsi, "processed"))
         expected_array = load_expectation(expected_array, type_="npy")
         tissue_mask = TissueMask()
-        np.testing.assert_array_almost_equal(tissue_mask(slide), expected_array)
+
+        slide_mask = tissue_mask(slide)
+
+        np.testing.assert_array_almost_equal(slide_mask, expected_array)
+
+    @pytest.mark.parametrize(
+        "tile, expected_array",
+        (
+            (
+                RGB.DIAGNOSTIC_SLIDE_THUMB_RGB,  # pen marks get considered as tissue
+                "mask-arrays/tissue-mask-diagnostic-slide-thumb-rgb",
+            ),
+            (
+                RGB.TCGA_LUNG_RGB,
+                "mask-arrays/tissue-mask-tcga-lung-rgb",
+            ),
+            (
+                TILES.TISSUE_LEVEL2_3000_7666_7096_11763,
+                "mask-arrays/tissue-mask-tissue-level2-3000-7666-7096-11763",
+            ),
+        ),
+    )
+    def it_can_construct_tissue_mask_tile(self, tile, expected_array):
+        tile = Tile(tile, None, None)
+        expected_array = load_expectation(expected_array, type_="npy")
+        tissue_mask = TissueMask()
+
+        tile_mask = tissue_mask(tile)
+
+        np.testing.assert_array_almost_equal(tile_mask, expected_array)
