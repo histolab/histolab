64866462a03e60f4e43d35d66ca81743c4d500a5
alessiamarcolini <98marcolini@gmail.com>
2021-09-16 17:39:01 +0200

    Calculate two principal components with eigenvectors decomposition

--- src/histolab/mixins.py
+++ src/histolab/mixins.py
@@ -6,3 +6,21 @@ class LinalgMixin:
     def normalize_columns(arr: np.ndarray) -> np.ndarray:
         """Normalize each column vector in an array"""
         return arr / np.linalg.norm(arr, axis=0)
+
+    @staticmethod
+    def two_principal_components(arr: np.ndarray) -> np.ndarray:
+        """
+        Return the two principal components of the covariance matrix of ``arr``.
+
+        Parameters
+        ----------
+        arr : np.ndarray
+            Input array
+
+        Returns
+        -------
+        np.ndarray
+            Two principal components.
+        """
+        _, V = np.linalg.eigh(np.cov(arr, rowvar=False))
+        return V[:, 1:3]
