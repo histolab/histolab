63c7c2a69bd79806f6216e88d835980d7e7659f3
Alessia Marcolini <98marcolini@gmail.com>
2022-07-19 08:54:05 +0000

    __version__ read from pyproject.toml

--- histolab/__init__.py
+++ histolab/__init__.py
@@ -15,3 +15,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ------------------------------------------------------------------------
+
+try:
+    from importlib import metadata
+except ImportError:  # for Python<3.8
+    import importlib_metadata as metadata
+
+__version__ = metadata.metadata("histolab")["Version"]
